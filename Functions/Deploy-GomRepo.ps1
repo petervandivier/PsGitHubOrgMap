821f878a9a8b1e475a5f7f0c3b51f6c176cf4ac4
PeterVandivier <peter.vandivier@gmail.com>
2023-06-22 15:56:26 -0400

    🚧 WIP `Deploy-Repo`

--- Functions/Deploy-GomRepo.ps1
+++ Functions/Deploy-GomRepo.ps1
@@ -1,2 +1,28 @@
 function Deploy-GomRepo {
+    [CmdletBinding(SupportsShouldProcess)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        'PSShouldProcess', 
+        '',
+        Justification = "Just passing the `-WhatIf` var through to child calls."
+    )]
+    Param(
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $OrganizationName,
+
+        [Parameter(Mandatory)]
+        [Alias('RepositoryName')]
+        [string]
+        $RepoName
+    )
+
+    $ExistingRepo = Get-GitHubRepository -OwnerName $OrganizationName -RepositoryName $RepoName
+
+    if($null -eq $ExistingRepo){
+        Write-Verbose "Deploying NEW repository '$RepoName' to organization '$OrganizationName'."
+        New-GitHubRepo
+    } else {
+        Set-GitHubRepository
+    }
 }

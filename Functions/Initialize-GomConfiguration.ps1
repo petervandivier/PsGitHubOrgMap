66f61943e7ba569fbaa37e5de9274a3d00e6b87a
PeterVandivier <peter.vandivier@gmail.com>
2023-06-22 14:00:24 -0400

    ✨orgname <> profile name

--- Functions/Initialize-GomConfiguration.ps1
+++ Functions/Initialize-GomConfiguration.ps1
@@ -5,10 +5,26 @@ function Initialize-GomConfiguration {
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [string]
-        $Name = 'Default'
+        $ProfileName = 'Default',
+
+        [Parameter(Mandatory)]
+        [string]
+        $OrganizationName
     )
+
+    $GhOrganization = Invoke-GHRestMethod -UriFragment "orgs/$OrganizationName" -Method Get -ErrorAction SilentlyContinue
+
+    if($null -eq $GhOrganization){
+        throw "No GitHub Organization found with name '$OrganizationName'. Are you using the correct credentials?"
+    }
 
     $Folder = New-Item -ItemType Directory -Path "$HOME/.PsGitHubOrgMap" -Force
 
-    New-Item -Path "${Folder}/${Name}.json" -Value "{}" -ErrorAction SilentlyContinue | Out-Null
+    New-Item -Path "${Folder}/${ProfileName}.json" -Value "{}" -ErrorAction SilentlyContinue | Out-Null
+
+
+    [PsCustomObject]@{
+        OrganizationName = $OrganizationName
+        ProfileName = $ProfileName
+    } | Export-Csv -Append "${Folder}/Organizations.csv" -UseQuotes AsNeeded
 }

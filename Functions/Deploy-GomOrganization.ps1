8a2a3b7d76a7f6c1f8e2087426ec4185c67cc1ec
PeterVandivier <peter.vandivier@gmail.com>
2023-06-27 15:03:16 -0400

    🔊 verbose stream `Deploy-Org`

--- Functions/Deploy-GomOrganization.ps1
+++ Functions/Deploy-GomOrganization.ps1
@@ -11,9 +11,20 @@ function Deploy-GomOrganization {
         $OrganizationName
     )
 
+    if($OrganizationName -ne $GomConfiguration.OrganizationName){
+        Write-Warning "Changing active GitHub Org Map configuration from '$($GomConfiguration.OrganizationName)' to '$OrganizationName'."
+        Import-GomConfiguration -OrganizationName $OrganizationName
+    }
+
+    $RepoRoot = Resolve-Path $GomConfiguration.Repository.Directory
+
+    Write-Verbose "Deploying assets for organization '$OrganizationName' from directory '$RepoRoot'."
+    Write-Verbose "Synchronizing users for organization '$OrganizationName'."
     Sync-GomUser -OrganizationName $OrganizationName
 
+    Write-Verbose "Synchronizing teams for organization '$OrganizationName'."
     Sync-GomTeam -OrganizationName $OrganizationName
 
+    Write-Verbose "Synchronizing repositories for organization '$OrganizationName'."
     Sync-GomRepo -OrganizationName $OrganizationName
 }

3cc9c1720300dbb30f21597d793977181468ec2b
PeterVandivier <peter.vandivier@gmail.com>
2023-08-01 15:19:41 -0400

    ✨ MVP Init-Config - 🦺 input validation - 💡 adds example in CBH - 📌adds module version-in-use to config output - 🐛 fixes upsert to organizations index

--- Functions/Initialize-GomConfiguration.ps1
+++ Functions/Initialize-GomConfiguration.ps1
@@ -1,5 +1,20 @@
 
 function Initialize-GomConfiguration {
+<#
+.Description
+    Given an existing local GomFolder, registers a local configuration
+    Assigning an OrgName & Repo to the directory.
+
+.Example
+    $GomConfig = @{
+        ProfileName = "DBTrenches"
+        OrgName = "DBTrenches"
+        Repo = "OrgMap"
+        Directory = "~/GitHub/DbTrenches.Orgmap"
+    }
+    Initialize-GomConfiguration @GomConfig
+
+#>
     [CmdletBinding()]
     param (
         [Parameter()]
@@ -8,8 +23,18 @@ function Initialize-GomConfiguration {
         $ProfileName = 'Default',
 
         [Parameter(Mandatory)]
+        [Alias('OrgName')]
         [string]
-        $OrganizationName
+        $OrganizationName,
+
+        [Parameter(Mandatory)]
+        [Alias('RepoName')]
+        [string]
+        $RepositoryName,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Directory
     )
 
     $GhOrganization = Invoke-GHRestMethod -UriFragment "orgs/$OrganizationName" -Method Get -ErrorAction SilentlyContinue
@@ -20,11 +45,42 @@ function Initialize-GomConfiguration {
 
     $Folder = New-Item -ItemType Directory -Path "$HOME/.PsGitHubOrgMap" -Force
 
-    New-Item -Path "${Folder}/${ProfileName}.json" -Value "{}" -ErrorAction SilentlyContinue | Out-Null
+    if(-Not (Test-Path $Directory)){
+        Write-Warning "Supplied directory '$Directory' could not be resolved. Config file content will not be valid."
+    }
 
-
-    [PsCustomObject]@{
+    $OrganizationConfig = [PsCustomObject]@{
+        PsGitHubOrgMap = [PsCustomObject]@{
+            ModuleVersion = (Get-Module PsGitHubOrgMap)[0].Version.ToString()
+        }
         OrganizationName = $OrganizationName
-        ProfileName = $ProfileName
-    } | Export-Csv -Append "${Folder}/Organizations.csv" -UseQuotes AsNeeded
+        Repository = [PsCustomObject]@{
+            Name = $RepositoryName
+            Directory = $Directory
+        }
+    } | ConvertTo-Json
+
+    $ConfigFile = "${Folder}/${ProfileName}.json"
+
+    if(Test-Path $ConfigFile){
+        $BackupConfigFile = "${Folder}/${ProfileName}_$(Get-Date -Format FileDateTime).json"
+        Write-Warning "Config file for profile '$ProfileName' already exists. Backing up to '$BackupConfigFile'."
+        Move-Item -Path $ConfigFile -Destination $BackupConfigFile
+    }
+
+    New-Item -Path $ConfigFile -Value $OrganizationConfig | Out-Null
+
+    $OrgIndex = "${Folder}/Organizations.csv"
+    $OrgList = Import-Csv $OrgIndex
+
+    if($OrgList.OrganizationName -Contains $OrganizationName) {
+        ($OrgList | Where-Object OrganizationName -eq $OrganizationName).ProfileName = $ProfileName
+    } else {
+        $OrgList += [PsCustomObject]@{
+            OrganizationName = $OrganizationName
+            ProfileName = $ProfileName
+        }
+    }
+
+    $OrgList | Export-Csv $OrgIndex -UseQuotes AsNeeded
 }
